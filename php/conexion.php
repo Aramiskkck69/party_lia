6d874e8adf7b91f7dc759a34b25603d66e8645db
Aramiskkck69 <jlaramis9@hotmail.com>
2020-12-04 14:59:49 -0700

    Antes de hacer el index.php

---
+++ php/conexion.php
@@ -0,0 +1,13 @@
+<?php
+//Conexion a la base de datos
+$conexion = mysqli_connect("localhost", 'root',"","party");
+
+    if (mysqli_connect_errno()){
+        echo "La conexion a la base de datos ha fallado" .mysqli_connect_error();
+    }
+    else{
+        echo "Conexion hecha correctamente" . "<br/>";
+    }
+// query para codificar caracteres
+    mysqli_query($conexion,"SET NAMES 'utf8'");
+?>
--- php/login.php
+++ php/login.php
@@ -1,30 +1,37 @@
 <?php
-//Conexion a la base de datos
-$conexion = mysqli_connect("localhost", 'root',"","party");
 
-    if (mysqli_connect_errno()){
-        echo "La conexion a la base de datos ha fallado" .mysqli_connect_error();
-    }
-    else{
-        echo "Conexion hecha correctamente" . "<br/>";
-    }
-// query para codificar caracteres
-    mysqli_query($conexion,"SET NAMES 'utf8'");
+require_once 'conexion.php';
+
+
+if(isset($_POST)){
+    $name = trim($_POST["userName"]);
+    $cell = trim($_POST["userCell"]);
+}
 // Consulta SELECT
-    $query = mysqli_query($conexion, "SELECT * FROM usuarios");
+    $query = mysqli_query($conexion, "SELECT * FROM usuarios WHERE telefono = '$cell'");
 
-    while($usuario = mysqli_fetch_assoc($query)){ //Convierte en un array la consulta
-        $NoInv = $usuario ['NoInv'] ;
-        $nombre = $usuario ['nombre'] ;
+//SELECT * FROM usuarios WHERE telefono = '$cell'
+
+    if ($query && mysqli_num_rows($query) == 1){
+        echo "El usuario existe" ."<br/>" ;
+            while($usuario = mysqli_fetch_assoc($query)){ //Convierte en un array la consulta
+                $NoInv = (int)$usuario ['NoInv'] ;
+                $cell_bd = (int)$usuario ['telefono'] ;
+                echo $NoInv ."<br/>";
+                echo $cell_bd."<br/>";
+            }
+        $query2 = mysqli_query($conexion, "SELECT * FROM invitados WHERE telefono_us = '$cell'");
+            while($usuario2 = mysqli_fetch_assoc($query2)){ //Convierte en un array la consulta
+                $nombre = $usuario2 ['nombre'] ;
+                $apellido = $usuario2 ['apellido'] ;
+                echo $nombre ."<br/>";
+                echo $apellido."<br/>";
+            }
     }
-    if ($nombre > 5){
-       echo "si jala este pedo weeee";
-   }
     else{
-        echo "el numero de invitados permitidos es: " . $NoInv ."<br/>";
+        echo "El usuario no existe" ."<br/>";
+        echo  "jejej xd";
     }
-
-    echo $nombre;
 
 ?>
 
